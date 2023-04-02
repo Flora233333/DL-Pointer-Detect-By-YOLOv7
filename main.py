37c95c3bab0cd45f7ebfe8ae97b31ba98324db4c
Flora <1005214199@qq.com>
2023-04-02 21:11:58 +0800

    基本测试完成

---
+++ main.py
@@ -0,0 +1,9 @@
+from yolov5_dnn import mult_test
+
+if __name__ == "__main__":
+    onnx_path = r'best-sim.onnx'
+    input_path = r'./input_image'
+    save_path = r'./output_image'
+
+    # video=True代表开启摄像头
+    mult_test(onnx_path, input_path, save_path, video=False)
